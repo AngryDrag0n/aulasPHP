4c45acabefd6702f0d68b6396f62e87a71574c63
gion <gionmarcos0609@gmail.com>
2024-09-03 14:51:35 -0300

    listas de exercicios 1

---
+++ listas/testes.php
@@ -0,0 +1,111 @@
+
+<?php 
+/*
+
+exercicio a 
+
+    $a = readline("digite um número: ");
+    $b = readline("digite um segundo numero: ");
+
+    $c = $a % $b;
+    $d = $a / $b;
+
+    echo "o resto é: " . $c . "\n";
+    echo "o quociente é: " . $d . "\n";
+
+*/
+
+
+/*
+    exercicio b 
+
+    $num1 = readline("Digite o primeiro número: ");
+    $num2 = readline("Digite o segundo número: ");
+    $num3 = readline("Digite o terceiro número: ");
+    $num4 = readline("Digite o quarto número: ");
+
+    $resultado = ($num1 + $num2 + $num3 + $num4)/4;
+    echo "O resultado é: " . $resultado;
+
+*/
+
+/*  exercicio c 
+
+    $prod = readline("Qual o valor do produto a ser reajustado? ");
+    $prodRea = $prod + $prod * 1.1 ;
+    echo $prodRea
+
+*/
+
+/*  exerciocio d 
+
+    $salario = readline("qual o seu salario: ");
+    $quant= readline("quantos quiloswatss você gastou: ");
+
+
+    $salarioMinimo=$salario/7;
+    $precoQuilo= $salario/100;
+
+    echo"Você paga a cada 100 quilowats R$ $precoQuilo \n";
+    $valorReais= $quant*$precoQuilo;
+    $desconto= $valorReais-($valorReais*(10/100));
+    echo"Valor a ser pago R$ $valorReais \n ";
+    echo "Valor com desocnto R$ $desconto\n ";
+
+*/
+
+/*  exercicio e 
+
+    $altura = readline("Digite a altura: ");
+    $base = readline("Digite a base: ");
+
+    $perimetro = ($altura * 2) + ($base * 2);
+    $area = $base * $altura;
+    $diagonal = sqrt($altura**2 + $base**2) ;
+
+    echo "O perimetro é $perimetro \n";
+    echo "A área é $area \n";
+    echo "A diagonal é $diagonal"
+
+*/
+
+/* exercicio f 
+
+    $altura = readline("digite a altura do triangulo: ");
+    $base = readline("digite a base do triangulo: ");
+    $resultado = ($base*$altura)/2;
+    echo $resultado
+*/
+
+/* exercicio g
+
+    $lmaior = readline("digite o lado maior do losango: ");
+    $lmenor = readline("digite o lado menor do losango: ");
+    $resultado = ($lmenor*$lmaior)/2;
+    echo $resultado
+*/
+
+/*  exercicio i(H foi excluido\:)
+
+    $quantcarros = readline("quantos carros você vendeu? ");
+    $valorvend = readline("Quantos em reais você vendeu? ");
+    $salariofix =readline("Qual o seu salario fixo? ");
+    $comissao = 100;
+    $salariofin = $salariofix + ($quantcarros*$comissao) + (0.05 * $valorvend);
+    echo $salariofin
+
+*/
+/*  exercicio j 
+    $hrs = readline("que horas são? ");
+    echo $hrs*60 . " minutos"
+*/
+
+/*  exercicio k 
+
+    $num = readline("Digite um numero: ");
+    echo strrev($num);
+*/
+
+
+?>
+
