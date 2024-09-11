1f60c0563bf40c73fa8aea4b16aeade8d31219d2
Gion <gionmarcos0609@gmail.com>
2024-09-11 16:45:59 -0300

    alguma coisa

---
+++ listas/lista-4/teste.php
@@ -0,0 +1,13 @@
+<?php 
+    echo "Para divisão digite '/' \n";
+    echo "Para multiplicação digite '*' \n";
+    echo "Para subtração digite '-' \n";
+    echo "Para adição digite '+' \n";
+    $operacao = readline("Oque você quer fazer com os números? \n ");
+
+    
+    function divisão(){
+
+    }
+    function multipli;
+?>
