b506de383aea0ef430a5df353d84f55ad72c9d37
Gion <gionmarcos0609@gmail.com>
2024-09-12 16:52:26 -0300

    mais exercicios

--- listas/lista-4/teste.php
+++ listas/lista-4/teste.php
@@ -1,13 +1,74 @@
-<?php 
-    echo "Para divisão digite '/' \n";
-    echo "Para multiplicação digite '*' \n";
-    echo "Para subtração digite '-' \n";
-    echo "Para adição digite '+' \n";
-    $operacao = readline("Oque você quer fazer com os números? \n ");
-
-    
-    function divisão(){
-
+<?php
+    /* exercicio 1
+    function soma($n1, $n2){
+        return $n1 + $n2;
     }
-    function multipli;
+    function diminuir($n1, $n2){
+        return $n1-$n2;
+    }
+    function dividir($n1, $n2){
+        return $n1/$n2;
+    }
+    function multiplicar($n1, $n2){
+        return $n1*$n2;
+    }
+    $n1 = readline("Digite o primeiro número: ");
+    $n2 = readline("Digite o segundo número: ");
+    echo "Digite + para somar \n"  ;
+    echo "Digite - para diminuir \n"  ;
+    echo "Digite / para dividir \n"  ;
+    echo "Digite * para dividir \n"  ;
+    echo "Oque deseja fazer com os números?: ";
+    $operacao = readline("");
+    if ($operacao == "+") {
+        echo soma($n1, $n2);
+    }
+    else if ($operacao == "-") {
+        echo diminuir($n1, $n2);
+    }
+    else if ($operacao == "/") {
+        echo dividir($n1, $n2);
+    }
+    else if ($operacao == "*") {
+        echo multiplicar($n1, $n2);
+    }*/
+    
+    /* exercicio 2 A)
+    function multiplo($num){
+        if ($num % 4 == 0){
+            echo "É divisivel ";
+            return true;
+        } else {
+            echo "False";
+            return false;
+        }
+    }
+    $num = readline("digite um número: ");
+    echo multiplo($num);
+*/
+    /* exercicio 2 B)
+
+    function somaTudo($minimo, $maximo) {
+        $resultado = 0; 
+        for ($i = $minimo; $i <= $maximo; $i++) {
+            $resultado += $i;
+        }
+        return $resultado;
+    }
+    
+    $minimo = readline("Defina um número inicial: ");
+    $maximo = readline("Defina um número máximo: ");
+    
+    $resultado = somaTudo($minimo, $maximo);
+    echo "A soma dos números de $minimo a $maximo é $resultado.\n";
+*/
+    /* exercicio 2 c)*/
+
+    function parImpar($num){
+        if ($num%2 == 0){
+            
+        } 
+    }
+
+
 ?>
